1cd455e327c78f2df0149db4fdf32d8e8ca45aa2
Akindu Himan <akinduhiman2@gmail.com>
2024-10-24 17:51:18 +0530

    API endpoints fixed

--- app/api/me/route.ts
+++ app/api/me/route.ts
@@ -23,7 +23,7 @@ export async function GET(request: Request) {
       return NextResponse.json({ message: 'Invalid token.' }, { status: 401 });
     }
 
-    return NextResponse.json({ username: decoded.username }, { status: 200 });
+    return NextResponse.json({ username: decoded.username, role: decoded.role }, { status: 200 });
   } catch (error: any) {
     console.error('Error fetching user data:', error);
     return NextResponse.json({ message: 'Internal Server Error.' }, { status: 500 });
